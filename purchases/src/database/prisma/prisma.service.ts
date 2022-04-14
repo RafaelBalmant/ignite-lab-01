287b1231960fc77bdfbd93b77984fd6a268ba284
rafael balmant <rafael.balmant@afya.com.br>
2022-04-13 22:29:03 -0300

    update

---
+++ purchases/src/database/prisma/prisma.service.ts
@@ -0,0 +1,29 @@
+import {
+  INestApplication,
+  Injectable,
+  OnModuleDestroy,
+  OnModuleInit,
+} from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  constructor() {
+    //call constructor of PrismaClient
+    super();
+  }
+  async onModuleInit() {
+    await this.$connect();
+  }
+  async onModuleDestroy() {
+    await this.$disconnect();
+  }
+  async enableShutdownHooks(app: INestApplication) {
+    this.$on('beforeExit', async () => {
+      await app.close();
+    });
+  }
+}
